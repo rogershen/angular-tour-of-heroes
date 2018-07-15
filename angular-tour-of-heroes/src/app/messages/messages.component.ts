7e9c23a64b45f2845bb356d434820d7fe873f727
Roger Shen <rogershen5@gmail.com>
2018-07-15 14:38:18 -0700

    initial commit

---
+++ angular-tour-of-heroes/src/app/messages/messages.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent implements OnInit {
+
+  constructor(private messageService: MessageService) { }
+
+  ngOnInit() {
+  }
+
+  clear() {
+    this.messageService.clear();
+  }
+
+  getMessages() {
+    return this.messageService.messages;
+  }
+}
